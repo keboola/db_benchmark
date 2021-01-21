dd246fda14f2ee89601dbe6226c965e9be46ef64
Petr Simecek <petr@keboola.com>
2021-01-21 11:37:09 +0100

    Update 009_new_rows_approach.sql

--- FINAL CHECK/009_new_rows_approach.sql
+++ FINAL CHECK/009_new_rows_approach.sql
@@ -41,7 +41,7 @@ WHERE  database_id > 4;
 
 /* LOAD DATA */
 
-CREATE TABLE [lineitems_tmp_hash] (
+CREATE TABLE [lineitems_dst] (
     [L_ORDERKEY]      nvarchar(4000) NOT NULL,
     [L_PARTKEY]       nvarchar(4000) NOT NULL DEFAULT NULL,
     [L_SUPPKEY]       nvarchar(4000) NOT NULL DEFAULT NULL,
@@ -64,7 +64,44 @@ WITH
     ,  DISTRIBUTION = HASH([L_ORDERKEY])
 ); 
 
-COPY INTO [lineitems_tmp_hash] FROM 'https://keboolabenchmark.blob.core.windows.net/padak/CSV/FILE_10M/TPCH_SF10/*.csv.gz'
+CREATE TABLE [lineitems_inc] (
+    [L_ORDERKEY]      nvarchar(4000) NOT NULL,
+    [L_PARTKEY]       nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_SUPPKEY]       nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_LINENUMBER]    nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_QUANTITY]      nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_EXTENDEDPRICE] nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_DISCOUNT]      nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_TAX]           nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_RETURNFLAG]    nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_LINESTATUS]    nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_SHIPDATE]      nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_COMMITDATE]    nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_RECEIPTDATE]   nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_SHIPINSTRUCT]  nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_SHIPMODE]      nvarchar(4000) NOT NULL DEFAULT NULL,
+    [L_COMMENT]       nvarchar(4000) NOT NULL DEFAULT NULL
+)
+WITH
+    (   HEAP
+    ,  DISTRIBUTION = HASH([L_ORDERKEY])
+); 
+
+
+COPY INTO [lineitems_dst] FROM 'https://keboolabenchmark.blob.core.windows.net/padak/CSV/FILE_10M/TPCH_SF1/*.csv.gz'
+WITH
+    (
+        FILE_TYPE='CSV',
+        CREDENTIAL =(IDENTITY ='Shared Access Signature', SECRET = '?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2021-01-22T18:28:10Z&st=2021-01-20T10:28:10Z&spr=https&sig=v2iAtTySRo4%2FiOCC9P9gZzdvKhHQFU7ije%2FC59%2FWCeg%3D'),
+        FIELDQUOTE='"',
+        FIELDTERMINATOR =',',
+        ENCODING = 'UTF8',
+        ROWTERMINATOR ='0x0A',
+        IDENTITY_INSERT = 'OFF' ,
+        FIRSTROW =2
+    ); --31.9s STATS_OFF, 33.9s STATS_ON
+
+COPY INTO [lineitems_inc] FROM 'https://keboolabenchmark.blob.core.windows.net/padak/CSV/FILE_10M/TPCH_SF10/*.csv.gz'
 WITH
     (
         FILE_TYPE='CSV',
@@ -118,7 +155,7 @@ FROM
        , [L_SHIPMODE]
        , [L_COMMENT]
        , ROW_NUMBER() OVER (PARTITION BY [L_ORDERKEY] ORDER BY [L_ORDERKEY]) AS "_row_number_"
-    FROM [lineitems_tmp_hash]) AS a
+    FROM [lineitems_dst]) AS a
 WHERE
     a."_row_number_" = 1
 ; --25s STATS_OFF, 28.2s STATS_ON
@@ -126,7 +163,7 @@ WHERE
 
 
 -- final UPDATE by the same amount of data (w/ & w/o PKEY)
-UPDATE lineitems_final_hash_ctas 
+UPDATE [lineitems_final_hash_ctas]
 SET 
   --L_ORDERKEY        = src.L_ORDERKEY, 
   L_PARTKEY         = src.L_PARTKEY,
@@ -145,7 +182,7 @@ SET
   L_SHIPMODE        = src.L_SHIPMODE, 
   L_COMMENT         = src.L_COMMENT,
   _timestamp        = '2020-12-18 15:06:35'
-FROM lineitems_tmp_hash AS src
+FROM lineitems_inc AS src
 WHERE
         lineitems_final_hash_ctas.L_ORDERKEY = src.L_ORDERKEY 
         /* AND
@@ -169,14 +206,14 @@ WHERE
 ;
 
 -- delete existing (already updated) rows
-DELETE [lineitems_final_hash_ctas]
+DELETE [lineitems_inc]
 WHERE
     EXISTS(
-            SELECT * FROM [lineitems_tmp_hash] 
-                WHERE [lineitems_final_hash_ctas].[L_ORDERKEY] = [lineitems_tmp_hash].[L_ORDERKEY]
+            SELECT * FROM [lineitems_inc] 
+                WHERE [lineitems_inc].[L_ORDERKEY] = [lineitems_final_hash_ctas].[L_ORDERKEY]
           );
 
-
+-- adding new rows only
 INSERT INTO
     [lineitems_final_hash_ctas] ([L_ORDERKEY], [L_PARTKEY], [L_SUPPKEY], [L_LINENUMBER], [L_QUANTITY],
                                                  [L_EXTENDEDPRICE], [L_DISCOUNT], [L_TAX], [L_RETURNFLAG],
@@ -199,4 +236,4 @@ INSERT INTO
                                                                                                                , [L_SHIPMODE]      AS [L_SHIPMODE]
                                                                                                                , [L_COMMENT]       AS [L_COMMENT]
                                                                                                                , '2020-12-18 15:06:35'
-FROM [lineitems_tmp_hash] AS [src]);          
+FROM [lineitems_inc] AS [src]);          
