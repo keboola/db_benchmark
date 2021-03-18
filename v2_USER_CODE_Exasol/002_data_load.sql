3e8f725ca70056b7650e4f2a207522b1fbdadfdc
Petr Simecek <petr@keboola.com>
2021-03-18 12:01:03 +0100

    exasol

---
+++ v2_USER_CODE_Exasol/002_data_load.sql
@@ -0,0 +1,96 @@
+/* DATA LOAD */
+
+create or replace stage "kbltst01"
+    url = 'azure://kbltst01.blob.core.windows.net/export'
+    credentials = (azure_sas_token = '?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2021-01-31T21:29:49Z&st=2021-01-06T13:29:49Z&spr=https,http&sig=xxx')
+    file_format = (
+        TYPE = 'CSV'
+            FIELD_DELIMITER = ','
+            FIELD_OPTIONALLY_ENCLOSED_BY = '\"'
+            COMPRESSION = 'GZIP'
+            TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS'
+            SKIP_HEADER = 1
+        )
+;
+
+
+copy into "call_center" from @kbltst01/call_center/
+;
+
+copy into "catalog_page" from @kbltst01/catalog_page/
+;
+
+copy into "catalog_returns" from @kbltst01/catalog_returns/
+;
+
+copy into "catalog_sales" from @kbltst01/catalog_sales/
+;
+
+copy into "customer" from @kbltst01/customer/
+;
+
+copy into "customer_address" from @kbltst01/customer_address/
+;
+
+copy into "customer_demographics" from @kbltst01/customer_demographics/
+;
+
+copy into "date_dim" from @kbltst01/date_dim/
+;
+
+copy into "household_demographics" from @kbltst01/household_demographics/
+;
+
+copy into "income_band" from @kbltst01/income_band/
+;
+
+copy into "inventory" from @kbltst01/inventory/
+;
+
+copy into "item" from @kbltst01/item/
+;
+
+copy into "promotion" from @kbltst01/promotion/
+;
+
+copy into "reason" from @kbltst01/reason/
+;
+
+copy into "ship_mode" from @kbltst01/ship_mode/
+;
+
+copy into "store" from @kbltst01/store/
+;
+
+copy into "store_returns" from @kbltst01/store_returns/
+;
+
+copy into "store_sales" from @kbltst01/store_sales/
+;
+
+copy into "time_dim" from @kbltst01/time_dim/
+;
+
+copy into "warehouse" from @kbltst01/warehouse/
+;
+
+copy into "web_page" from @kbltst01/web_page/
+;
+
+copy into "web_returns" from @kbltst01/web_returns/
+;
+
+copy into "web_sales" from @kbltst01/web_sales/
+;
+
+copy into "web_site" from @kbltst01/web_site/
+;
+
+/*
+-- list queries to get duration
+select substr(query_text, 0, 50), start_time, compilation_time, execution_time, total_elapsed_time 
+from table(information_schema.query_history_by_warehouse(result_limit => 200))
+order by start_time;
+*/
+
+-- 7min46s (LARGE DWH)
---
+++ v2_USER_CODE_Exasol/003_warmup.sql
@@ -0,0 +1,194 @@
+-- 11.7s
+
+-- warmup_01.sql
+select *
+    from call_center
+    where cc_call_center_sk = -100
+    order by cc_call_center_id desc
+    limit 100
+;
+
+-- warmup_02.sql
+select *
+    from catalog_page
+    where cp_catalog_page_sk = -100
+    order by cp_catalog_page_id desc
+    limit 100
+;
+
+-- warmup_03.sql
+select *
+    from catalog_returns
+    where cr_returned_date_sk = -100
+    order by cr_returned_date_sk desc
+    limit 100
+;
+
+-- warmup_04.sql
+select *
+    from catalog_sales
+    where cs_sold_date_sk = -100
+    order by cs_sold_date_sk desc
+    limit 100
+;
+
+-- warmup_05.sql
+select *
+    from customer_address
+    where ca_address_sk = -100
+    order by ca_address_id desc
+    limit 100
+;
+
+-- warmup_06.sql
+select *
+    from customer_demographics
+    where cd_demo_sk = -100
+    order by cd_demo_sk desc
+    limit 100
+;
+
+-- warmup_07.sql
+select *
+    from customer
+    where c_customer_sk = -100
+    order by c_customer_id desc
+    limit 100
+;
+
+-- warmup_08.sql
+select *
+    from date_dim
+    where d_date_sk = -100
+    order by d_date_id desc
+    limit 100
+;
+
+-- warmup_09.sql
+select *
+    from household_demographics
+    where hd_demo_sk = -100
+    order by hd_demo_sk desc
+    limit 100
+;
+
+-- warmup_10.sql
+select *
+    from income_band
+    where ib_income_band_sk = -100
+    order by ib_income_band_sk desc
+    limit 100
+;
+
+-- warmup_11.sql
+select *
+    from inventory
+    where inv_item_sk = -100
+    order by inv_item_sk desc
+    limit 100
+;
+
+-- warmup_12.sql
+select *
+    from item
+    where i_item_sk = -100
+    order by i_item_id desc
+    limit 100
+;
+
+-- warmup_13.sql
+select *
+    from promotion
+    where p_promo_sk = -100
+    order by p_promo_id desc
+    limit 100
+;
+
+-- warmup_14.sql
+select *
+    from reason
+    where r_reason_sk = -100
+    order by r_reason_id desc
+    limit 100
+;
+
+-- warmup_15.sql
+select *
+    from ship_mode
+    where sm_ship_mode_sk = -100
+    order by sm_ship_mode_id desc
+    limit 100
+;
+
+-- warmup_16.sql
+select *
+    from store_returns
+    where sr_returned_date_sk = -100
+    order by sr_returned_date_sk desc
+    limit 100
+;
+
+-- warmup_17.sql
+select *
+    from store_sales
+    where ss_sold_date_sk = -100
+    order by ss_sold_date_sk desc
+    limit 100
+;
+
+-- warmup_18.sql
+select *
+    from store
+    where s_store_sk = -100
+    order by s_store_id desc
+    limit 100
+;
+
+-- warmup_19.sql
+select *
+    from time_dim
+    where t_time_sk = -100
+    order by t_time_id desc
+    limit 100
+;
+
+-- warmup_20.sql
+select *
+    from warehouse
+    where w_warehouse_sk = -100
+    order by w_warehouse_id desc
+    limit 100
+;
+
+-- warmup_21.sql
+select *
+    from web_page
+    where wp_web_page_sk = -100
+    order by wp_web_page_id desc
+    limit 100
+;
+
+-- warmup_22.sql
+select *
+    from web_returns
+    where wr_returned_date_sk = -100
+    order by wr_returned_date_sk desc
+    limit 100
+;
+
+-- warmup_23.sql
+select *
+    from web_sales
+    where ws_sold_date_sk = -100
+    order by ws_sold_date_sk desc
+    limit 100
+;
+
+-- warmup_24.sql
+select *
+    from web_site
+    where web_site_sk = -100
+    order by web_site_id desc
+    limit 100
+;
+
